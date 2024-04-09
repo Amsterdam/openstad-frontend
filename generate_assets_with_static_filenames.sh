44cf1f98ee771c177f2eee8da36665e12ea34f89
IanR01 <ian.r.89@gmail.com>
2024-04-09 11:55:57 +0200

    also set static filenames for 'user-' prefix files

--- generate_assets_with_static_filenames.sh
+++ generate_assets_with_static_filenames.sh
@@ -7,15 +7,19 @@ node apostrophe.js apostrophe:generation --create-bundle assets
 assets_directory="assets/public/apos-minified"
 generation_id_file="assets/data/generation"
 
-# Get the filenames starting with "anon-" and ending with ".css" and ".js"
-css_file=$(find "$assets_directory" -type f -name "anon-*.css" -print -quit)
-js_file=$(find "$assets_directory" -type f -name "anon-*.js" -print -quit)
+# Get the filenames starting with "anon-" and "user-" and ending with ".css" and ".js"
+anon_css_file=$(find "$assets_directory" -type f -name "anon-*.css" -print -quit)
+anon_js_file=$(find "$assets_directory" -type f -name "anon-*.js" -print -quit)
+user_css_file=$(find "$assets_directory" -type f -name "anon-*.css" -print -quit)
+user_js_file=$(find "$assets_directory" -type f -name "anon-*.js" -print -quit)
 
 # Check if environment variables are set and files are found
 if [[ -n $STATIC_ASSETS_FILENAME_CONSTANT && -n $css_file && -n $js_file ]]; then
     # Rename the files with the values from environment variables
-    mv "$css_file" "$assets_directory/anon-$STATIC_ASSETS_FILENAME_CONSTANT.css"
-    mv "$js_file" "$assets_directory/anon-$STATIC_ASSETS_FILENAME_CONSTANT.js"
+    mv "$anon_css_file" "$assets_directory/anon-$STATIC_ASSETS_FILENAME_CONSTANT.css"
+    mv "$anon_js_file" "$assets_directory/anon-$STATIC_ASSETS_FILENAME_CONSTANT.js"
+    mv "$user_css_file" "$assets_directory/user-$STATIC_ASSETS_FILENAME_CONSTANT.css"
+    mv "$user_js_file" "$assets_directory/user-$STATIC_ASSETS_FILENAME_CONSTANT.js"
     echo "$STATIC_ASSETS_FILENAME_CONSTANT" > "$generation_id_file"
     echo "Files renamed successfully."
 elif [[ -z $CSS_FILENAME || -z $JS_FILENAME ]]; then
